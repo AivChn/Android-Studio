26c85f5d1c96dd86ff577ac1c229bb31a2d201d0
Amishav <amishavhere@gmail.com>
2024-03-04 10:38:03 +0200

    1st question

--- app/src/main/java/net/aiv/Amishav/LoginActivity.java
+++ app/src/main/java/net/aiv/Amishav/LoginActivity.java
@@ -3,6 +3,7 @@ package net.aiv.Amishav;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,12 @@ public class LoginActivity extends AppCompatActivity {
         username = findViewById(R.id.username);
         login = findViewById(R.id.loginButton);
 
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String usernameStr = prefs.getString("username", "");
+        if (!usernameStr.isEmpty()) {
+            username.setText(usernameStr);
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +36,10 @@ public class LoginActivity extends AppCompatActivity {
                 if (usernameStr.isEmpty())
                     Toast.makeText(LoginActivity.this, "No username entered", Toast.LENGTH_SHORT).show();
                 else {
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("username", usernameStr);
+                    editor.apply();
+
                     Intent intent = new Intent(LoginActivity.this, ExerciseActivity.class);
                     intent.putExtra("username", usernameStr);
 
